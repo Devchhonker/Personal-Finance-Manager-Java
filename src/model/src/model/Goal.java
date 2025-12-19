990167dc03bac7fb8cf89e6ba57cfe13291e6467
Devchhonker <devchhonker600@gmail.com>
2025-12-19 15:48:55 +0530

    Create Goal.java

---
+++ src/model/src/model/Goal.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Goal {
+    private String description;
+    private double targetAmount;
+
+    public Goal(String description, double targetAmount) {
+        this.description = description;
+        this.targetAmount = targetAmount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getTargetAmount() {
+        return targetAmount;
+    }
+}
